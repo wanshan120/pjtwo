1c7e75f3f0898cd016986b388736df1fc71cf5a3
wanshan120 <wanshan120@gmail.com>
2022-06-11 05:18:32 +0900

    yarn fix で出たlint修正

--- app_front/app/src/App.tsx
+++ app_front/app/src/App.tsx
@@ -3,15 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 const App: FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
+  <div className="app">
+    <header className="app-header">
+      <img src={logo} className="app-logo" alt="logo" />
       <p>
         Edit
         <code>src/App.tsx</code>
         and save to reload.
       </p>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+      <a className="app-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
         Learn React
       </a>
     </header>
