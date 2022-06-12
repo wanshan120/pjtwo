33ec70bc831d072de27319ffdff04e93152e4cc6
wanshan120 <wanshan120@gmail.com>
2022-06-12 11:51:09 +0900

    eslint error修復　jsx-no-useless-fragment

--- app_front/app/src/components/organisms/CharactersList.tsx
+++ app_front/app/src/components/organisms/CharactersList.tsx
@@ -9,32 +9,28 @@ type Props = {
   isLoading?: boolean;
 };
 
-const CharactersList: FC<Props> = ({ characters = [], isLoading = false }) => (
-  <>
-    <h2>a</h2>
-    {isLoading ? (
-      <Spinner />
-    ) : (
-      <Item.Group>
-        {characters.map((character) => (
-          <Item key={character.id}>
-            <Icon name="user circle" size="huge" />
-            <Item.Content>
-              <Item.Header>{character.name}</Item.Header>
-              <Item.Meta>
-                {character.grade}
-                年生
-              </Item.Meta>
-              <Item.Meta>
-                {character.height ? character.height : '???'}
-                cm
-              </Item.Meta>
-            </Item.Content>
-          </Item>
-        ))}
-      </Item.Group>
-    )}
-  </>
-);
+const CharactersList: FC<Props> = ({ characters = [], isLoading = false }) =>
+  isLoading ? (
+    <Spinner />
+  ) : (
+    <Item.Group>
+      {characters.map((character) => (
+        <Item key={character.id}>
+          <Icon name="user circle" size="huge" />
+          <Item.Content>
+            <Item.Header>{character.name}</Item.Header>
+            <Item.Meta>
+              {character.grade}
+              年生
+            </Item.Meta>
+            <Item.Meta>
+              {character.height ? character.height : '???'}
+              cm
+            </Item.Meta>
+          </Item.Content>
+        </Item>
+      ))}
+    </Item.Group>
+  );
 
 export default CharactersList;
