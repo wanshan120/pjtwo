355121e253dad7a31cc0cde4dd8507c45e23ec1b
wanshan120 <wanshan120@gmail.com>
2022-08-31 04:23:21 +0900

    CORS設定追加（ローカルホストの設定のみ）

--- api_main/v1/routers.go
+++ api_main/v1/routers.go
@@ -3,6 +3,9 @@ package routers
 import (
 	"api_main/v1/movie"
 
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -19,7 +22,34 @@ func InitRouter() *gin.Engine {
 
 // アプリケーション設定の初期化
 func setUpConfig(router *gin.Engine) {
-
+	// CORS
+	router.Use(cors.New(cors.Config{
+		// アクセスを許可したいアクセス元
+		AllowOrigins: []string{
+			`http://localhost:3000`,
+		},
+		// アクセスを許可したいHTTPメソッド(以下の例だとPUTやDELETEはアクセスできません)
+		AllowMethods: []string{
+			"POST",
+			"GET",
+			"PUT",
+			"DELETE",
+			"PATCH",
+		},
+		// 許可したいHTTPリクエストヘッダ
+		AllowHeaders: []string{
+			"Access-Control-Allow-Credentials",
+			"Access-Control-Allow-Headers",
+			"Content-Type",
+			"Content-Length",
+			"Accept-Encoding",
+			"Authorization",
+		},
+		// cookieなどの情報を必要とするかどうか
+		AllowCredentials: true,
+		// preflightリクエストの結果をキャッシュする時間
+		MaxAge: 24 * time.Hour,
+	}))
 	// redis session
 }
 
